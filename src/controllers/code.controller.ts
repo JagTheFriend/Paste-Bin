c55ad5c8dc3435395e3d0aea2de918f783c6eed7
JagTheFriend <jagthefriend12@gmail.com>
2021-11-28 11:06:17 +0200

    Added a way todisplay the line number

--- src/controllers/code.controller.ts
+++ src/controllers/code.controller.ts
@@ -2,13 +2,17 @@ import { NextFunction, Request, Response } from 'express';
 
 class CodeController {
   public index = (req: Request, res: Response, next: NextFunction) => {
+    const code = `idk
+    what to
+    write here
+    `;
     try {
       res
         .set(
           'Content-Security-Policy',
           "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'",
         )
-        .render('index');
+        .render('index', { code: code });
     } catch (error) {
       next(error);
     }
