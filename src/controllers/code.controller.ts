4064e15679e187688084dcd25528937a3cbf0bf9
JagTheFriend <jagthefriend12@gmail.com>
2021-11-28 10:55:38 +0200

    Fixed bug

--- src/controllers/code.controller.ts
+++ src/controllers/code.controller.ts
@@ -3,7 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 class CodeController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.render('index');
+      res
+        .set(
+          'Content-Security-Policy',
+          "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'",
+        )
+        .render('index');
     } catch (error) {
       next(error);
     }
